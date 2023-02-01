ee8ae521e8f5392bf68b226f1a73533d6eeaa4bb
David Esteban Gallego <66768539+Dagapa@users.noreply.github.com>
2023-02-01 01:14:31 -0500

    bugs fixed

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -3,38 +3,35 @@ import Image from 'next/image';
 import AppContext from '@context/AppContext';
 import addToCartImage from '@icons/bt_add_to_cart.svg';
 import addedToCartImage from '@icons/bt_added_to_cart.svg';
+
 import styles from '@styles/ProductItem.module.scss';
 
 const ProductItem = ({ product }) => {
 	const { state, addToCart } = useContext(AppContext);
 
-	const handleClick = item => {
+	const handleClick = (item) => {
 		console.log('in cart: ', state.cart.includes(item));
 		addToCart(item);
 	};
 
+	if (!product.images[0]) {
+		return null;
+	}
+
 	return (
-		<div className={styles.ProductItem}>
-			<Image
-				src={product.images[0]}
-				width={240} height={240} alt={product.title}
-				loader={() => product.images[0]}
-			/>
+		<div className={styles['ProductItem']}>
+			{<Image loader={() => product.images[0]} src={product.images[0]} alt={product?.title} width={240} height={240} />}
 			<div className={styles['product-info']}>
 				<div>
 					<p>${product.price}</p>
 					<p>{product.title}</p>
 				</div>
-				<figure
-					className={styles['more-clickable-area']}
-					onClick={() => handleClick(product)}
-					aria-hidden="true"
-				>
-					{state.cart.includes(product) ? <Image
-						className={`${styles.disabled} ${styles['add-to-cart-btn']}`}
-						src={addedToCartImage}
-						alt="added to cart"
-					/> : <Image className={`${styles['add-to-cart-btn']} ${styles.pointer}`} src={addToCartImage} alt="add to cart" />}
+				<figure className={styles['more-clickable-area']} onClick={() => handleClick(product)} onKeyPress={() => handleClick(product)} role="presentation">
+					{state.cart.includes(product) ? (
+						<Image className={(styles['disabled'], styles['add-to-cart-btn'])} src={addedToCartImage} width={50} height={50} alt="added to cart" />
+					) : (
+						<Image className={styles['add-to-cart-btn pointer']} src={addToCartImage} alt="add to cart" />
+					)}
 				</figure>
 			</div>
 		</div>
